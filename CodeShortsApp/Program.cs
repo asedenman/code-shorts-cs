2a5848711b73293baccbb3976eddca671c979736
asedenman <cKcN16du!p0>
2021-11-07 12:52:34 +0000

    - initial commit

---
+++ CodeShortsApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeShortsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
